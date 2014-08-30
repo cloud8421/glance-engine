49555b5764c01ac28689275dd638564d68197ccf
Claudio Ortolina <claudio@new-bamboo.co.uk>
2014-08-30 08:57:55 +0100

    Adds dependencies.

--- mix.exs
+++ mix.exs
@@ -12,7 +12,7 @@ defmodule Glance.Mixfile do
   #
   # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger],
+    [applications: [:logger, :cowboy, :plug, :httpoison],
      mod: {Glance, []}]
   end
 
@@ -26,6 +26,10 @@ defmodule Glance.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    []
+    [
+      {:cowboy, "~> 1.0.0"},
+      {:plug, "~> 0.6.0"},
+      {:httpoison, "~> 0.4.1"}
+    ]
   end
 end
